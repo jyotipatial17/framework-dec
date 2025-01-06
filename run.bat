a328a745b4b5d39d703728616bb75aca5cc87b08
Jyoti Patial <jasminethakur.joy1@gmail.com>
2025-01-06 12:37:13 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ pip install selenium
 pip install pytest-html
 pip install pytest-xdist
 pip install openpyxl
-pip install allure-pytest
-pytest -v -s testcase\test_001_accountregistration.py --browser chrome
-#pytest -v -s testcase\test_002_search_excel.py --browser chrome
+# pip install allure-pytest
+#pytest -v -s testcase\test_001_accountregistration.py --browser chrome
+pytest -v -s testcase\test_002_search_excel.py --browser chrome
 # pytest -v -s testcase --browser chrome
